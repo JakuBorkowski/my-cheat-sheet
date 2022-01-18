24d022732274a2d57790e124a0161ee0f590ad92
Jakub Borkowski <jakub.bor2@gmail.com>
2022-01-18 21:43:33 +0100

    simplify code crate uniwersal function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import './App.css';
-import Element from './element';
+import "./App.css";
+import Element from "./element";
 
 function App() {
   //tablica z danymi
   const data = [
-    {name:"git", id:1,text:"lorem", data:[] },
-    {name: "java script", id:2, text:"też lorem", data:[
-      {name: "tablice", id:[2,1], text:"jeszcze inne lorem", data:[]},
-      {name: "pętle", id:[2,2], text:"to samo lorem", data:[]}
-    ]}
-  ]
+    { name: "git", id: 1, text: "text git", data: [] },
+    {
+      name: "java script",
+      id: 2,
+      text: "text js",
+      data: [
+        {
+          name: "tablice",
+          id: [2, 1],
+          text: "text tablice",
+          data: [
+            {
+              name: "tworzenie nowych tablic",
+              id: [2, 1, 1],
+              text: "tekst tworzenie",
+              data: [],
+            },
+            {
+              name: "kopiowanie tablic",
+              id: [2, 1, 2],
+              text: "text kopiowanie",
+              data: [],
+            },
+          ],
+        },
+        { name: "pętle", id: [2, 2], text: "tekst pętle", data: [] },
+      ],
+    },
+  ];
 
-  return (
-   <>
-   {/*mapa danych która przypisuje przy użyciu propsów dane z tablicy*/}
-  {data.map((data)=>{return <Element name={data.name} text={data.text} key={data.id} 
-  element={data.data.map((dat)=>{return <Element name={dat.name} text={dat.text} key={dat.id} element={null}/>})
-  }/>})}
-   </>
-  );
+  const mapElement = (data) => {
+    return (
+      <Element
+        name={data.name}
+        text={data.text}
+        key={data.id}
+        element={data.data.map(mapElement)}
+      />
+    );
+  };
+
+  return <>{data.map(mapElement)}</>;
 }
-
-
-
 export default App;
