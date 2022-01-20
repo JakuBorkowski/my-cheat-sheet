755fd5ad93b4e4917756a80a446e9820cd6dc12d
Jakub Borkowski <jakub.bor2@gmail.com>
2022-01-20 16:12:59 +0100

    create json with data, download data to app.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Element from "./element";
 
 function App() {
-  //tablica z danymi
-  const data = [
-    { name: "--git", id: 1, text: "text git", data: [] },
-    {
-      name: "{java script}",
-      id: 2,
-      text: "text js",
-      data: [
-        {
-          name: "tablice",
-          id: [2, 1],
-          text: "text tablice",
-          data: [
-            {
-              name: "tworzenie nowych tablic",
-              id: [2, 1, 1],
-              text: "tekst tworzenie",
-              data: [],
-            },
-            {
-              name: "kopiowanie tablic",
-              id: [2, 1, 2],
-              text: "text kopiowanie",
-              data: [],
-            },
-          ],
-        },
-        { name: "pętle", id: [2, 2], text: "tekst pętle", data: [] },
-      ],
-    },
-  ];
 
+  const [data, setData] = useState([])
+
+  useEffect( ()=>{
+    fetch("data.json")
+    .then(response => {
+
+      if (response.ok){
+        return response.json()
+      }
+      throw response;
+    })
+    .then(data=>{
+      setData(data);
+    })
+  },
+  []
+  )
   const mapElement = (data) => {
     return (
       <Element
@@ -45,6 +32,7 @@ function App() {
     );
   };
 
-  return <>{data.map(mapElement)}</>;
+  return <>{data.map(mapElement)} 
+  </>;
 }
 export default App;
